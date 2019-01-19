0e72d9acc8efc0107015634d636a2382a683a1df
KimHyeshin <hyeshin2530@gmail.com>
2019-01-19 23:45:40 +0900

    add : router-view 사용. component 분리.

---
+++ src/components/Page404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Page 404 : Page not found
+  </div>
+</template>
+
+<script>
+  export default {
+    name : 'Page404'
+  }
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import App from '../App.vue'
 
 Vue.use(VueRouter); // 미들웨어 추가
 
-const Login = { template: '<div>Login Page</div>'}
-const Page404 = { template: '<div>404 Page not found</div>'}
+// 컴포넌트 가지고오기
+import Home from '../components/Home.vue'
+import Login from '../components/Login.vue'
+import Page404 from '../components/Page404.vue'
+
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
-      {path: '/', component: App},
+      {path: '/', component: Home},
       {path: '/login', component: Login},
       {path: '*', component: Page404}
     ]
