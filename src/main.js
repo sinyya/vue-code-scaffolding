9fffc92578a5cc7be7e1693ec1c3280c82ff6cc1
KimHyeshin <hyeshin2530@gmail.com>
2019-01-19 23:26:02 +0900

    add : vue-router 추가

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter); // 미들웨어 추가
 
 const Login = { template: '<div>Login Page</div>'}
 const Page404 = { template: '<div>404 Page not found</div>'}
 
-const routes = {
-  '/': App,
-  '/login': Login
-}
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+      {path: '/', component: App},
+      {path: '/login', component: Login},
+      {path: '*', component: Page404}
+    ]
+  }
+)
 
 new Vue({
   el: '#app',
-  computed:{
-    VueComponent() {
-      return routes[window.location.pathname] || Page404
-    }
-  },
-  render(h) {
-   return h(this.VueComponent)
-  }
+  router,
+  render : h => h({template: '<router-view></router-view>'})
 })
